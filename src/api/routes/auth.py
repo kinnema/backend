3ff3eef7a13583bb68abb7b0b73cadf5b6ab4f17
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-06-23 18:21:30 +0200

    fix: login after register

--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.api.deps import CurrentUser, SessionDep
 from src.core.config import settings
@@ -12,23 +13,9 @@ from src.schemas import LoginResponse, UserCreate, UserPublic
 
 router = APIRouter()
 
-
-@router.post("/register", response_model=UserPublic)
-async def register(*, session: SessionDep, user: UserCreate):
-    user_db = await AuthCrud.get_user_by_email(session=session, email=user.email)
-
-    if user_db:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    return await AuthCrud.create_user(session=session, user=user)
-
-
-@router.post("/login")
-async def login(
-    *, session: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-) -> LoginResponse:
+async def login_function(session: AsyncSession, email: str, password: str) -> LoginResponse:
     user = await AuthCrud.authenticate(
-        session=session, email=form_data.username, password=form_data.password
+        session=session, email=email, password=password
     )
 
     if not user:
@@ -41,6 +28,30 @@ async def login(
         access_token=create_access_token(user.id, expires_delta=access_token_expires),
         user=UserPublic.from_orm(user),
     )
+
+
+@router.post("/register")
+async def register(*, session: SessionDep, user: UserCreate) -> LoginResponse:
+    user_db = await AuthCrud.get_user_by_email(session=session, email=user.email)
+
+    if user_db:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+
+    await AuthCrud.create_user(session=session, user=user)
+
+    login = await login_function(session=session, email=user.email, password=user.password)
+
+    print(login)
+
+    return login
+
+
+@router.post("/login")
+async def login(
+    *, session: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> LoginResponse:
+    return await login_function(session=session, email=form_data.username, password=form_data.password)
 
 
 @router.get("/me", response_model=UserPublic)
